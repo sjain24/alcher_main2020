e08e395f3ac40c8f20198e26973ce56bb7dc9a7e
sjain24 <jain170107062@iitg.ac.in>
2020-01-23 19:19:01 +0530

    Team page + events individual pages

--- vendor/bootstrap/js/custom.js
+++ vendor/bootstrap/js/custom.js
@@ -59,6 +59,231 @@ $('.vogueslide').owlCarousel({
     }
 })
 
+$('.classslide').owlCarousel({
+    loop:false,
+    smartSpeed:850,
+    margin:0,
+    mouseDrag:true,
+    touchDrag:true,
+    autoplay:false,
+    stagePadding: 70,
+    autoplayTimeout:2000,
+    nav: true,
+    navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+$('.musicslide').owlCarousel({
+    loop:false,
+    smartSpeed:850,
+    margin:0,
+    mouseDrag:true,
+    touchDrag:true,
+    autoplay:false,
+    stagePadding: 70,
+    autoplayTimeout:2000,
+    nav: true,
+    navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+$('.literaryslide').owlCarousel({
+    loop:false,
+    smartSpeed:850,
+    margin:0,
+    mouseDrag:true,
+    touchDrag:true,
+    autoplay:false,
+    stagePadding: 70,
+    autoplayTimeout:2000,
+    nav: true,
+    navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+$('.artslide').owlCarousel({
+    loop:false,
+    smartSpeed:850,
+    margin:0,
+    mouseDrag:true,
+    touchDrag:true,
+    autoplay:false,
+    stagePadding: 70,
+    autoplayTimeout:2000,
+    nav: true,
+    navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+$('.dramaslide').owlCarousel({
+    loop:false,
+    smartSpeed:850,
+    margin:0,
+    mouseDrag:true,
+    touchDrag:true,
+    autoplay:false,
+    stagePadding: 70,
+    autoplayTimeout:2000,
+    nav: true,
+    navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+$('.danceslide').owlCarousel({
+    loop:false,
+    smartSpeed:850,
+    margin:0,
+    mouseDrag:true,
+    touchDrag:true,
+    autoplay:false,
+    stagePadding: 70,
+    autoplayTimeout:2000,
+    nav: true,
+    navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+$('.sportslide').owlCarousel({
+    loop:false,
+    smartSpeed:850,
+    margin:0,
+    mouseDrag:true,
+    touchDrag:true,
+    autoplay:false,
+    stagePadding: 70,
+    autoplayTimeout:2000,
+    nav: true,
+    navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+$('.digitalslide').owlCarousel({
+    loop:false,
+    smartSpeed:850,
+    margin:0,
+    mouseDrag:true,
+    touchDrag:true,
+    autoplay:false,
+    stagePadding: 70,
+    autoplayTimeout:2000,
+    nav: true,
+    navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+$('.movieslide').owlCarousel({
+    loop:false,
+    smartSpeed:850,
+    margin:0,
+    mouseDrag:true,
+    touchDrag:true,
+    autoplay:false,
+    stagePadding: 70,
+    autoplayTimeout:2000,
+    nav: true,
+    navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
 $(" .vogueslide .owl-next").click(function(){
     $(".vogueslide .prev-slide").removeClass("hide");
   $(".vogueslide .prev-slide").addClass("movenav");
@@ -72,6 +297,22 @@ $(".vogueslide .owl-prev").click(function(){
   $(".vogueslide .next-slide").removeClass("hide");
   $(".vogueslide .next-slide").addClass("movenavs");
 });
+
+// for class apart //
+
+/*$(" .classslide .owl-next").click(function(){
+    $(".classslide .prev-slide").removeClass("hide");
+  $(".classslide .prev-slide").addClass("movenav");
+  $(".classslide .next-slide").addClass("hide");
+  $(".classslide .next-slide").removeClass("movenavs");
+});
+
+$(".classslide .owl-prev").click(function(){
+  $(".classslide .prev-slide").addClass("hide");
+  $(".classslide .prev-slide").removeClass("movenav");
+  $(".classslide .next-slide").removeClass("hide");
+  $(".classslide .next-slide").addClass("movenavs");
+});*/
 
 
 
@@ -96,7 +337,7 @@ $('#menuhide').click(function(e){
             if ((scrollHeight - scrollPosition) / scrollHeight === 0) {
                 // when scroll to bottom of the page
             }
-             console.log(" Hi scrollPosition  "+scrollPosition );
+            //  console.log(" Hi scrollPosition  "+scrollPosition );
              if( scrollPosition>1854 ){
 
                    $( ".al_wing" ).removeClass( "displaynone" );                
